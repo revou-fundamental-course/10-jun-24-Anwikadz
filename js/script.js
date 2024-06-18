0be7e583310c077e3f2a914468a8480c4bc41581
Anwikadz <anwikadz67@gmail.com>
2024-06-18 17:49:33 +0800

    init nambah java prototype1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 /*Ini JavaScript*/
 
+
+function convert() {
+    const celcius = document.getElementById('celcius').value;
+    const fahrenheit = (celcius * 9/5) + 32;
+    document.getElementById('fahrenheit').value = fahrenheit;
+}
+
+function reset() {
+    document.getElementById('celcius').value = '';
+    document.getElementById('fahrenheit').value = '';
+}
+
+function reverse() {
+    const fahrenheit = document.getElementById('fahrenheit').value;
+    const celcius = (fahrenheit - 32) * 5/9;
+    document.getElementById('celcius').value = celcius;
+}
